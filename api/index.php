8b8e2f37f226cfabbe178cf8ad5a51027f40ee5f
farras55 <farraskuyy@gmail.com>
2026-06-07 22:04:09 +0700

    tes3

--- api/index.php
+++ api/index.php
@@ -1,2 +1,32 @@
 <?php
+
+// Trik ini memaksa direktori storage dan cache Laravel
+// agar tidak perlu izin menulis yang rumit di Vercel
+function createStorageDirs()
+{
+    $storageDirs = [
+        '/tmp/storage/app/public',
+        '/tmp/storage/framework/cache/data',
+        '/tmp/storage/framework/sessions',
+        '/tmp/storage/framework/testing',
+        '/tmp/storage/framework/views',
+        '/tmp/storage/logs',
+        '/tmp/bootstrap/cache',
+    ];
+
+    foreach ($storageDirs as $dir) {
+        if (!file_exists($dir)) {
+            mkdir($dir, 0777, true);
+        }
+    }
+    
+    // Memberi tahu Laravel di mana lokasi storage dan cache yang baru
+    $_ENV['APP_STORAGE'] = '/tmp/storage';
+    putenv('APP_STORAGE=/tmp/storage');
+}
+
+// Jalankan fungsi pembuatan direktori sementara sebelum memuat aplikasi
+createStorageDirs();
+
+// Ini adalah baris asli yang kita butuhkan
 require __DIR__ . '/../public/index.php';
